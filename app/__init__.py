54f57273b15bd3f439d13809cd64a9d4e4fa865b
Hung-Ta Chen <htcchen@ucdavis.edu>
2024-05-31 03:23:27 -0700

    Update __init__.py for mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from pymongo import MongoClient
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,10 +12,14 @@ migrate = Migrate()
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={r"/api/*": {"origins": "*"}})  # for cross platform
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # Initialize MongoDB connection
+    mongo_client = MongoClient(app.config['MONGO_URI'])
+    mongo_db = mongo_client['db_name']
 
     from .places.routes import place_blueprint, review_blueprint
     app.register_blueprint(place_blueprint, url_prefix='/api')
